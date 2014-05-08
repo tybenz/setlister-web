e80ed77ebf796f838473d5088d80e69a89370494
tybenz <tabenziger@gmail.com>
2014-05-08 16:55:55 -0700

    labels

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
 $( function() {
-    $( 'body' ).scrollspy( { target: '.master-list-col' } );
-
-    $( '.list-group' ).affix({
-        offset: {
-            top: 90
-        }
-    });
-
     $( '.tab' ).on( 'click', function( evt ) {
         evt.preventDefault();
 
