10697a5ddc27568e0bc145b0bfa9369de522c610
tybenz <tabenziger@gmail.com>
2014-05-08 08:20:15 -0700

    moving along

--- js/app.js
+++ js/app.js
@@ -6,4 +6,19 @@ $( function() {
             top: 90
         }
     });
+
+    $( '.tab' ).on( 'click', function( evt ) {
+        evt.preventDefault();
+
+        var $this = $( this );
+        $( '.sidebar' ).toggleClass( 'slide-in' );
+
+        if ( $this.hasClass( 'glyphicon-chevron-right' ) ) {
+            $this.addClass( 'glyphicon-chevron-left' );
+            $this.removeClass( 'glyphicon-chevron-right' );
+        } else {
+            $this.addClass( 'glyphicon-chevron-right' );
+            $this.removeClass( 'glyphicon-chevron-left' );
+        }
+    });
 });
